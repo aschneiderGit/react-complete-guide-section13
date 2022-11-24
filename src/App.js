10a1f15439984fc2e60a8f387fe623bb15c86e8a
Aimé Schneider <34367419+aschneiderGit@users.noreply.github.com>
2022-11-24 12:05:54 +0100

    Ad UserFinder

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
+import UserFinder from './components/UserFinder';
 import Users from './components/Users';
 
 function App() {
-  return (
-    <div>
-      <Users />
-    </div>
-  );
+	return (
+		<div>
+			<UserFinder />
+		</div>
+	);
 }
 
 export default App;
